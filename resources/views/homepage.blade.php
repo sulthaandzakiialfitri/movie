a1ef32d26c28650ddd195d4aa1ce07d667456b76
sulthaandzakiialfitri <dzakisultan309@gmail.com>
2026-04-28 11:17:21 +0700

    refactor: improve code quality and readability

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -17,13 +17,13 @@
         <div class="card mb-3" style="max-width: 540px;">
             <div class="row g-0">
               <div class="col-md-4">
-                <img src="/images/{{ $movie['foto_sampul'] }}" class="img-fluid rounded-start" alt="...">
+                <img src="/images/{{ $movie->foto_sampul }}" class="img-fluid rounded-start" alt="...">
             </div>
                 <div class="col-md-8">
                     <div class="card-body">
-                        <h5 class="card-title">{{ $movie['judul'] }}</h5>
-                        <p class="card-text">{{ $movie['sinopsis'] }}</p>
-                        <a href="/movie/{{ $movie['id'] }}" class="btn btn-success">Lihat Selanjutnya</a>
+                        <h5 class="card-title">{{ $movie->judul }}</h5>
+                        <p class="card-text">{{ $movie->sinopsis }}</p>
+                        <a href="/movie/{{ $movie->id }}" class="btn btn-success">Lihat Selanjutnya</a>
                     </div>
                 </div>
             </div>
